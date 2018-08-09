07755ae86fca34f313478abea00ba39d8ba6b34e
MediBoss <mediassumani49@gmail.com>
2018-08-08 20:22:24 -0400

    better scripting

--- Phone_Number/phone_script.py
+++ Phone_Number/phone_script.py
@@ -9,29 +9,14 @@ import os
 import re
 
 # Function to detect if a string passed as a parameter is a phone number
-def isPhoneNumber(text):
+def isPhoneNumber(data):
 
-    first_phone_exp = re.compile(r'(\d{3}-)?\d{3}-\d{4})')# represents the nnn-nnn-nnn cell phone format with the an area code optional
-    second_phone_exp = re.compile(r'\d{3}.\d{3}.\d{4}') # represents the nnn.nnn.nnn cell phone format
-    third_phone_expx = re.compile(r'(\(\d\d\d\)) (\d\d\d-\d\d\d\d)') # represents the  (nnn) nnn-nnnn cell phone format
-
-        #Checking properties of a standard phone number
-    if len(text) != 12:
-        return False
-    for data in range(0,3):
-        if isinstance(data, int) is False:
-            return False
-    if text[3] != '-':
-        return False
-    for data in range(4,7):
-        if isinstance(data, int) != True:
-            return false
-    if text[7] != '-':
-        return False
-    for data in range(8,12):
-        if isinstance(data, int) != True:
-            return False
-    return True
+    #first_phone_exp = re.compile(r'(\d{3}-)?\d{3}-\d{4}')# represents the nnn-nnn-nnn cell phone format with the an area code optional
+    #second_phone_exp = re.compile(r'\d{3}.\d{3}.\d{4}') # represents the nnn.nnn.nnn cell phone format
+    #third_phone_expx = re.compile(r'(\(\d\d\d\)) (\d\d\d-\d\d\d\d)') # represents the  (nnn) nnn-nnnn cell phone format
+    results = []
+    phone_exp = re.compile(r'\d{3}-\d{3}-\d{4}|\d{3}.\d{3}.\d{4}')
+    return results.append(phone_exp.findall(data))
 
 # Function to parse through the text file
 def parse_article():
@@ -39,10 +24,7 @@ def parse_article():
     try:
         with open(file_path,'r') as article:
             for current_line in article:
-                for index in range(len(current_line)):
-                    chunk_of_data = current_line[index:index+12]
-                    if isPhoneNumber(chunk_of_data):
-                        print('Phone number found : {}'.format(chunk_of_data))
+                isPhoneNumber(line)
 
     except IOError:
         print("ERROR FOUND : FILE COULD NOT BE OPENED")
